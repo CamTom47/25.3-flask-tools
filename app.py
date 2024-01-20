aedb9d458c4ab3926a734a045ebdc15cfa52659c
Cameron Thomas <camthomas47@gmail.com>
2024-01-20 12:36:14 -0500

    Session functionality added for survey responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -10,15 +10,16 @@ debug = DebugToolbarExtension(app)
 
 responses = [];
 
-@app.route('/test')
+@app.route('/', methods=["POST"])
 def survey_start():
+    session['respsonses'] = []; 
+    
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('survey_start.html', title = title, instructions = instructions)
 
 @app.route('/questions/<int:idx>')
 def handle_question(idx = 0):
-    
     if(idx < len(satisfaction_survey.questions) and idx == len(responses)):
         idx = idx
         choices = satisfaction_survey.questions[idx].choices
@@ -31,6 +32,7 @@ def handle_question(idx = 0):
         return redirect(f'/questions/{idx_correction}')
 
     else:
+        raise
         return render_template('thank_you.html')
 
     
@@ -41,5 +43,6 @@ def next_question():
     idx = int(request.form['idx'])
     response = request.form['choice']
     responses.append(response)
+    session['responses'] = responses
     idx += 1
     return redirect(f'/questions/{idx}')
