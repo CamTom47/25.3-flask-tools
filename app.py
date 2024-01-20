72c245ab5dc1555e2a1184056f9c7c785e82a9fb
Cameron Thomas <camthomas47@gmail.com>
2024-01-19 20:36:51 -0500

    25.3 Exercise completed

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, request, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+responses = [];
+
+@app.route('/test')
+def survey_start():
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('survey_start.html', title = title, instructions = instructions)
+
+@app.route('/questions/<int:idx>')
+def handle_question(idx = 0):
+    
+    if(idx < len(satisfaction_survey.questions) and idx == len(responses)):
+        idx = idx
+        choices = satisfaction_survey.questions[idx].choices
+        question = satisfaction_survey.questions[idx].question
+        return render_template('questions.html' , choices = choices, idx = idx, question = question)
+    
+    elif(idx > len(responses)):
+        idx_correction = len(responses)
+        flash('Please answer questions in the correct order')
+        return redirect(f'/questions/{idx_correction}')
+
+    else:
+        return render_template('thank_you.html')
+
+    
+
+
+@app.route('/questions/next', methods=["POST"])
+def next_question():
+    idx = int(request.form['idx'])
+    response = request.form['choice']
+    responses.append(response)
+    idx += 1
+    return redirect(f'/questions/{idx}')
